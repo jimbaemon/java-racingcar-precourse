ac255e84404b495eb50df7ce9e7cafe1e258f2ae
jimbaemon <jimbae@kakao.com>
2022-10-10 19:40:19 +0900

    feat: 자동차가 전진조건에 충족하면 전진하도록 구현.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -6,12 +6,25 @@ public class Car {
     private final static int MAX_NAME_LENGTH = 5;
 
     private final String name;
+    private Location location;
 
     public Car(String name) {
-        if(name.length() > MAX_NAME_LENGTH){
+        if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("자동차의 이름은 5자를 초과할 수 없습니다.");
         }
         this.name = name;
+        this.location = new Location();
+    }
+
+    public Car(String name, Location location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public void move(MovingStrategy movingStrategy) {
+        if (movingStrategy.isMovable()) {
+            this.location = location.add();
+        }
     }
 
     @Override
@@ -23,11 +36,11 @@ public class Car {
             return false;
         }
         Car car = (Car) o;
-        return Objects.equals(name, car.name);
+        return Objects.equals(name, car.name) && Objects.equals(location, car.location);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(name, location);
     }
 }
